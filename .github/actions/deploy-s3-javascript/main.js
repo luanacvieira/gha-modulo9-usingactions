7fe2968917ffd1f5421e864a5dc3aefa833365f1
luanacvieira <luanacvieira@users.noreply.github.com>
2024-05-20 00:14:29 -0300

    JS action, AWSZ deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,7 +4,10 @@ const exec = require('@actions/exec');
 
 function run() {
     try {
-    
+        const bucket = core.getInput('bucket-name', { required: true });
+        const bucketRegion = core.getInput('bucket-region', { required: true });
+        const distFolder = core.getInput('dist-folder', { required: true });
+        
         core.notice('Hello from my custom javscript action!!');
     
     } catch (error) {
