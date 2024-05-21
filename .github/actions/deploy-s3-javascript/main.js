9ef5b22261dde3cf6b5408e6eeac93a3b74718ae
luanacvieira <luanacvieira@users.noreply.github.com>
2024-05-20 21:01:35 -0300

    teste repo docker

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,18 +1,19 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    try {
-        const bucket = core.getInput('bucket-name', { required: true });
-        const bucketRegion = core.getInput('bucket-region', { required: true });
-        const distFolder = core.getInput('dist-folder', { required: true });
-        
-        core.notice('Hello from my custom javscript action!!');
-    
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+  // 1) Get some input values
+  const bucket = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2) Upload files
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('website-url', websiteUrl); // ::set-output
 }
 
 run();
